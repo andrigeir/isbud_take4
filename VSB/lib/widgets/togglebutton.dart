1d8f3b78d5f243b1dbfd85a0b9190701e63793d0
Andri Geir Anarson <s164518@student.dtu.dk>
2020-10-13 18:01:32 +0200

    Toogle button update

--- VSB/lib/widgets/togglebutton.dart
+++ VSB/lib/widgets/togglebutton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:customtogglebuttons/customtogglebuttons.dart';
 
 class MyToggleButton extends StatefulWidget {
   @override
@@ -7,35 +8,68 @@ class MyToggleButton extends StatefulWidget {
 
 class _MyToggleButtonState extends State<MyToggleButton> {
   List<bool> _isSelected = [false, false, false];
+
+  List<Color> _color = [
+    Colors.white.withOpacity(0.5),
+    Colors.white.withOpacity(0.5),
+    Colors.white.withOpacity(0.5),
+  ];
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ToggleButtons(
-        children: <Widget>[
-          Icon(Icons.bluetooth),
-          Icon(Icons.wifi),
-          Icon(Icons.flash_on),
+        child: CustomToggleButtons(
+            children: <Widget>[
+          Container(
+            width: 90,
+            child: FloatingActionButton.extended(
+              heroTag: null,
+              onPressed: null,
+              label: Text('Small'),
+              backgroundColor: _color[0],
+            ),
+          ),
+          Container(
+            width: 90,
+            child: FloatingActionButton.extended(
+              heroTag: null,
+              onPressed: null,
+              label: Text('Medium'),
+              backgroundColor: _color[1],
+            ),
+          ),
+          Container(
+            width: 90,
+            child: FloatingActionButton.extended(
+              heroTag: null,
+              onPressed: null,
+              label: Text('Large'),
+              backgroundColor: _color[2],
+            ),
+          ),
         ],
-        isSelected: _isSelected,
-        onPressed: (int index) {
-          setState(() {
-            _isSelected = [false, false, false];
-            _isSelected[index] = !_isSelected[index];
-          });
-        },
-        // region example 1
-        color: Colors.grey,
-        selectedColor: Colors.red,
-        fillColor: Colors.lightBlueAccent,
+            isSelected: _isSelected,
+            onPressed: (int index) {
+              setState(() {
+                _isSelected = [false, false, false];
+                _isSelected[index] = !_isSelected[index];
+                _color = [
+                  Colors.white.withOpacity(0.5),
+                  Colors.white.withOpacity(0.5),
+                  Colors.white.withOpacity(0.5)
+                ];
+                _color[index] = Theme.of(context).accentColor;
+              });
+            },
+            borderColor: Theme.of(context).primaryColor,
+            selectedColor: Theme.of(context).primaryColor,
+            color: Theme.of(context).primaryColor,
+            selectedBorderColor: Theme.of(context).primaryColor,
+            spacing: 10,
+            borderWidth: 0,
+            elevation: 0,
+            unselectedFillColor: Theme.of(context).primaryColor)
+
         // endregion
-        // region example 2
-        borderColor: Colors.lightBlueAccent,
-        selectedBorderColor: Colors.red,
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
-        ),
-        // endregion
-      ),
-    );
+        );
   }
 }
